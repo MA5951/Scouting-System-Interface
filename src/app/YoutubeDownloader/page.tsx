330d188b15bdcf3bb25ca5d96fa3702d05aa9776
Asaf Meizner <75739196+AsafMeizner@users.noreply.github.com>
2023-10-23 13:44:27 +0300

    cleaner code for download

--- src/app/YoutubeDownloader/page.tsx
+++ src/app/YoutubeDownloader/page.tsx
@@ -1,3 +1,4 @@
+// src/app/YoutubeDownloader/page.tsx
 "use client"
 
 import React, { useState } from 'react';
@@ -6,79 +7,89 @@ const YoutubeDownloader = () => {
   const [url, setUrl] = useState('');
 
   const downloadVideo = async () => {
-    const response = await fetch('YoutubeDownloader/api/download', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        url,
-        type: 'video',
-      }),
-    });
+    try {
+      const response = await fetch('/YoutubeDownloader/api/download', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          url,
+          type: 'video',
+        }),
+      });
 
-    if (response.ok) {
-      const blob = await response.blob();
-      const url = window.URL.createObjectURL(blob);
-      const a = document.createElement('a');
-      a.href = url;
-      a.download = 'video.mp4';
-      document.body.appendChild(a);
-      a.click();
-      document.body.removeChild(a);
-      window.URL.revokeObjectURL(url);
-    } else {
-      alert('Error downloading video');
+      if (response.ok) {
+        const blob = await response.blob();
+        const url = window.URL.createObjectURL(blob);
+        const a = document.createElement('a');
+        a.href = url;
+        a.download = 'video.mp4';
+        document.body.appendChild(a);
+        a.click();
+        document.body.removeChild(a);
+        window.URL.revokeObjectURL(url);
+      } else {
+        alert('Error downloading video');
+      }
+    } catch (error) {
+      console.error('Error:', error);
+      alert('An error occurred');
     }
   };
 
   const downloadSound = async () => {
-    const response = await fetch('YoutubeDownloader/api/download', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        url,
-        type: 'sound',
-      }),
-    });
+    try {
+      const response = await fetch('/YoutubeDownloader/api/download', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          url,
+          type: 'sound',
+        }),
+      });
 
-    if (response.ok) {
-      const blob = await response.blob();
-      const url = window.URL.createObjectURL(blob);
-      const a = document.createElement('a');
-      a.href = url;
-      a.download = 'audio.mp3';
-      document.body.appendChild(a);
-      a.click();
-      document.body.removeChild(a);
-      window.URL.revokeObjectURL(url);
-    } else {
-      alert('Error downloading audio');
+      if (response.ok) {
+        const blob = await response.blob();
+        const url = window.URL.createObjectURL(blob);
+        const a = document.createElement('a');
+        a.href = url;
+        a.download = 'audio.mp3';
+        document.body.appendChild(a);
+        a.click();
+        document.body.removeChild(a);
+        window.URL.revokeObjectURL(url);
+      } else {
+        alert('Error downloading audio');
+      }
+    } catch (error) {
+      console.error('Error:', error);
+      alert('An error occurred');
     }
   };
 
-    return (
-        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', height: '100vh' }}>
-            <div style={{ color: 'white', textAlign: 'center' }}>
-                <h1 style={{ textAlign: 'center', marginBottom: '0px', fontSize: '48px' }}>YouTube Downloader</h1>
-                <input
-                type="text"
-                value={url}
-                onChange={(e) => setUrl(e.target.value)}
-                placeholder="Enter YouTube URL"
-                style={{color: 'white', marginRight: '10px', marginBottom: '10px', backgroundColor: 'rgb(30, 31, 34)', padding: '10px', borderRadius: '5px', border: 'none'}}
-                />
-                <button className="purpleButton" onClick={downloadVideo} style={{ marginRight: '10px' }}>
-                Download Video
-                </button>
-                <button className="purpleButton" onClick={downloadSound}>
-                Download MP3
-                </button>
-            </div>
-        </div>
-    );
+  return (
+    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', height: '100vh' }}>
+      <div style={{ color: 'white', textAlign: 'center' }}>
+        <h1 style={{ textAlign: 'center', marginBottom: '0px', fontSize: '48px' }}>YouTube Downloader</h1>
+        <input
+          type="text"
+          value={url}
+          onChange={(e) => setUrl(e.target.value)}
+          placeholder="Enter YouTube URL"
+          style={{color: 'white', marginRight: '10px', marginBottom: '10px', backgroundColor: 'rgb(30, 31, 34)', padding: '10px', borderRadius: '5px', border: 'none'}}
+        />
+      </div>
+      <button className="purpleButton" onClick={downloadVideo} style={{ marginRight: '10px' }}>
+        Download Video
+      </button>
+      <button className="purpleButton" onClick={downloadSound}>
+        Download MP3
+      </button>
+    </div>
+  );
 };
 
 export default YoutubeDownloader;
--- src/app/YoutubeDownloader/server.ts
+++ src/app/YoutubeDownloader/server.ts
@@ -1,37 +1,29 @@
+// src/app/YoutubeDownloader/server.ts
 "use server"
 
 import { NextApiRequest, NextApiResponse } from 'next';
 import ytdl from 'ytdl-core';
 import ffmpeg from 'fluent-ffmpeg';
 
-const downloadVideoByUrl = async (url: string, res: NextApiResponse) => {
+const downloadByUrl = async (url: string, type: string, res: NextApiResponse) => {
   try {
     const info = await ytdl.getInfo(url);
     const title = info.videoDetails.title;
 
-    const format = ytdl.chooseFormat(info.formats, { quality: 'highest' });
-
-    res.setHeader('Content-Disposition', `attachment; filename="${title}.mp4"`);
+    let format;
+    if (type === 'video') {
+      format = ytdl.chooseFormat(info.formats, { quality: 'highest' });
+      res.setHeader('Content-Disposition', `attachment; filename="${title}.mp4"`);
+    } else if (type === 'sound') {
+      format = ytdl.chooseFormat(info.formats, { quality: 'highestaudio' });
+      res.setHeader('Content-Disposition', `attachment; filename="${title}.mp3"`);
+    } else {
+      throw new Error('Invalid type');
+    }
 
     ytdl(url, { format }).pipe(res);
   } catch (error) {
-    res.status(500).json({ error: 'Error downloading video' });
-  }
-};
-
-const downloadSoundByUrl = async (url: string, res: NextApiResponse) => {
-  try {
-    const info = await ytdl.getInfo(url);
-    const title = info.videoDetails.title;
-
-    const format = ytdl.chooseFormat(info.formats, { quality: 'highestaudio' });
-
-    res.setHeader('Content-Disposition', `attachment; filename="${title}.mp3"`);
-
-    ytdl(url, { format })
-      .pipe(ffmpeg().input('pipe:0').outputFormat('mp3').pipe(res));
-  } catch (error) {
-    res.status(500).json({ error: 'Error downloading audio' });
+    res.status(500).json({ error: `Error downloading ${type}` });
   }
 };
 
@@ -43,11 +35,5 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     return;
   }
 
-  if (type === 'video') {
-    await downloadVideoByUrl(url, res);
-  } else if (type === 'sound') {
-    await downloadSoundByUrl(url, res);
-  } else {
-    res.status(400).json({ error: 'Invalid type' });
-  }
+  await downloadByUrl(url, type, res);
 };
