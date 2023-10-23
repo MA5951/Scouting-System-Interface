2cb5c7fc320212712e6de21739371001d4c66f8f
Asaf Meizner <75739196+AsafMeizner@users.noreply.github.com>
2023-10-23 13:57:42 +0300

    returned to earlier clearer version of the code

--- src/app/YoutubeDownloader/server.ts
+++ src/app/YoutubeDownloader/server.ts
@@ -1,49 +1,53 @@
-// src/app/YoutubeDownloader/server.ts
 "use server"
 
 import { NextApiRequest, NextApiResponse } from 'next';
 import ytdl from 'ytdl-core';
 import ffmpeg from 'fluent-ffmpeg';
 
-const downloadByUrl = async (url: string, type: string, res: NextApiResponse) => {
-	try {
-		const info = await ytdl.getInfo(url);
-		const title = info.videoDetails.title;
+const downloadVideoByUrl = async (url: string, res: NextApiResponse) => {
+  try {
+    const info = await ytdl.getInfo(url);
+    const title = info.videoDetails.title;
 
-		let format;
-		if (type === 'video') {
-			format = ytdl.chooseFormat(info.formats, { quality: 'highest' });
-			res.setHeader('Content-Disposition', `attachment; filename="${title}.mp4"`);
-		} else if (type === 'sound') {
-			format = ytdl.chooseFormat(info.formats, { quality: 'highestaudio' });
-			res.setHeader('Content-Disposition', `attachment; filename="${title}.mp3"`);
-		} else {
-			throw new Error('Invalid type');
-		}
+    const format = ytdl.chooseFormat(info.formats, { quality: 'highest' });
 
-		const stream = ytdl(url, { format });
+    res.setHeader('Content-Disposition', `attachment; filename="${title}.mp4"`);
 
-		if (type === 'sound') {
-			// Pipe through ffmpeg for audio
-			ffmpeg(stream)
-				.format('mp3')
-				.audioBitrate(128)
-				.pipe(res, { end: true });
-		} else {
-			stream.pipe(res);
-		}
-	} catch (error) {
-		res.status(500).json({ error: `Error downloading ${type}` });
-	}
+    ytdl(url, { format }).pipe(res);
+  } catch (error) {
+    res.status(500).json({ error: 'Error downloading video' });
+  }
+};
+
+const downloadSoundByUrl = async (url: string, res: NextApiResponse) => {
+  try {
+    const info = await ytdl.getInfo(url);
+    const title = info.videoDetails.title;
+
+    const format = ytdl.chooseFormat(info.formats, { quality: 'highestaudio' });
+
+    res.setHeader('Content-Disposition', `attachment; filename="${title}.mp3"`);
+
+    ytdl(url, { format })
+      .pipe(ffmpeg().input('pipe:0').outputFormat('mp3').pipe(res));
+  } catch (error) {
+    res.status(500).json({ error: 'Error downloading audio' });
+  }
 };
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-	const { url, type } = req.body;
+  const { url, type } = req.body;
 
-	if (!url || !type) {
-		res.status(400).json({ error: 'Missing URL or type' });
-		return;
-	}
+  if (!url || !type) {
+    res.status(400).json({ error: 'Missing URL or type' });
+    return;
+  }
 
-	await downloadByUrl(url, type, res);
+  if (type === 'video') {
+    await downloadVideoByUrl(url, res);
+  } else if (type === 'sound') {
+    await downloadSoundByUrl(url, res);
+  } else {
+    res.status(400).json({ error: 'Invalid type' });
+  }
 };
