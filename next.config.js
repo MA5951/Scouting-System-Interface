1771c837748fcf64dc29ddc3f78168e71da65b3f
Asaf Meizner <75739196+AsafMeizner@users.noreply.github.com>
2023-12-11 22:17:55 +0200

    fix new

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 
 const nextConfig = {
     images: {
-        domains: ['https://raw.githubusercontent.com', 'raw.githubusercontent.com', 'github.com', 'githubusercontent.com', 'githubusercontent'],
+        domains: ['https://raw.githubusercontent.com', 'raw.githubusercontent.com', 'github.com', 'githubusercontent.com', 'githubusercontent', 'https://raw.githubusercontent.com/AsafMeizner/multi-app-website-new/master/public/galgol.JPG'],
     },
 }
 
