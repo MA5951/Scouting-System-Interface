ce3a1cc9980ed0ea9b7db9c266ef3690a553d7de
Asaf Meizner <75739196+AsafMeizner@users.noreply.github.com>
2023-12-19 20:54:13 +0200

    tested new condition for navbar when redirecting

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        {!pathname.includes('urlShortner/') && <Navbar />}
+        {pathname.includes('/urlShortner') && pathname != '/urlShortner' && <Navbar />}
         {children}
       </body>
     </html>
