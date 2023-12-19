081771d359a738b5d03332c708c44a497208de60
Asaf Meizner <75739196+AsafMeizner@users.noreply.github.com>
2023-12-19 20:39:59 +0200

    test

---
+++ src/app/urlShortner/layout.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import React, { useState } from 'react';
+import { Inter } from 'next/font/google';
+import './globals.css';
+import router from 'next/router';
+import { Navbar } from '@/src/components/Navbar';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        {!router.pathname.includes('urlShortner/') && <Navbar />}
+        {children}
+      </body>
+    </html>
+  );
+}
