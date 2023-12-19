b6ce9f4668f613ecb7a7fe04e5e07422e784758e
Asaf Meizner <75739196+AsafMeizner@users.noreply.github.com>
2023-12-19 22:02:19 +0200

    dfsfs

--- src/app/YoutubeDownloader/server.ts
+++ src/app/YoutubeDownloader/server.ts
@@ -7,29 +7,33 @@ import ytdl from 'ytdl-core';
 export default async function downloadHandler(req: NextApiRequest, res: NextApiResponse) {
   res.setHeader('Access-Control-Allow-Origin', '*'); // Allow all origins (for testing only)
 
-  if (req.method !== 'POST') {
-    res.status(405).end(); // Method Not Allowed
-    return;
-  }
+  if (req.method === 'GET') {
+    // If it's a GET request, render a dummy page or provide an informative message
+    res.status(200).send('This is a download endpoint. Use POST requests to initiate downloads.');
+  } else if (req.method === 'POST') {
+    // If it's a POST request, handle the download logic
+    const { url, type } = req.body;
 
-  const { url, type } = req.body;
+    try {
+      if (!url || !type) {
+        res.status(400).json({ error: 'Missing URL or type' });
+        return;
+      }
 
-  try {
-    if (!url || !type) {
-      res.status(400).json({ error: 'Missing URL or type' });
-      return;
+      const videoInfo = await ytdl.getBasicInfo(url);
+      const format = type === 'video' ? ytdl.chooseFormat(videoInfo.formats, { quality: 'highest' }) : ytdl.chooseFormat(videoInfo.formats, { quality: 'highestaudio' });
+      const videoStream = ytdl(url, { format });
+
+      res.setHeader('Content-Disposition', `attachment; filename="${videoInfo.videoDetails.title}.${type === 'video' ? 'mp4' : 'mp3'}"`);
+      res.setHeader('Content-Type', type === 'video' ? 'video/mp4' : 'audio/mp3');
+
+      videoStream.pipe(res);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: `Error downloading ${type === 'video' ? 'video' : 'audio'}` });
     }
-
-    const videoInfo = await ytdl.getBasicInfo(url);
-    const format = type === 'video' ? ytdl.chooseFormat(videoInfo.formats, { quality: 'highest' }) : ytdl.chooseFormat(videoInfo.formats, { quality: 'highestaudio' });
-    const videoStream = ytdl(url, { format });
-
-    res.setHeader('Content-Disposition', `attachment; filename="${videoInfo.videoDetails.title}.${type === 'video' ? 'mp4' : 'mp3'}"`);
-    res.setHeader('Content-Type', type === 'video' ? 'video/mp4' : 'audio/mp3');
-
-    videoStream.pipe(res);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: `Error downloading ${type === 'video' ? 'video' : 'audio'}` });
+  } else {
+    // If it's neither GET nor POST, return Method Not Allowed
+    res.status(405).end();
   }
 }
