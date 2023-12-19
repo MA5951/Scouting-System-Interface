c3bd0bcd5c9caf20d6ceed212beb017f20fdc43c
Asaf Meizner <75739196+AsafMeizner@users.noreply.github.com>
2023-12-19 20:48:54 +0200

    testing usePathname

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import React, { useState } from 'react';
 import { Inter } from 'next/font/google';
 import './globals.css';
 import { Navbar } from '../components/Navbar';
+import { usePathname } from 'next/navigation';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -12,11 +13,11 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
-
+  const pathname = usePathname();
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Navbar />
+        {!pathname.includes('urlShortner/') && <Navbar />}
         {children}
       </body>
     </html>
