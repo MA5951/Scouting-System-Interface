3fc7f5adaf63ce2d67cc37fb365e6c8ef254d77f
Asaf Meizner <75739196+AsafMeizner@users.noreply.github.com>
2023-12-19 22:05:29 +0200

    test

--- src/app/YoutubeDownloader/page.tsx
+++ src/app/YoutubeDownloader/page.tsx
@@ -11,7 +11,7 @@ const YoutubeDownloader = () => {
 	  try {
 		const form = document.createElement('form');
 		form.method = 'POST';
-		form.action = 'https://catblik.tech/YoutubeDownloader/api/download';
+		form.action = 'https://catblik.tech/YoutubeDownloader';
 		form.target = '_blank'; // Open the response in a new tab/window
   
 		const urlInput = document.createElement('input');
